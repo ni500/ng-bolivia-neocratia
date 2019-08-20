155e079e6bdc9d61aeaf25030a4a68018549e116
Nicolas Contreras <nicolas@pappcorn.com>
2019-08-20 16:24:35 -0500

    Material Imports on share-module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,31 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {
-  MatToolbarModule,
-  MatSidenavModule,
-  MatCheckboxModule,
-  MatIconModule,
-  MatButtonModule,
-  MatListModule
-} from '@angular/material';
-import { FlexLayoutModule } from '@angular/flex-layout';
-
-import { FormsModule } from '@angular/forms';
-
-const MaterialModules = [
-  MatToolbarModule,
-  MatSidenavModule,
-  MatCheckboxModule,
-  MatIconModule,
-  MatButtonModule,
-  MatListModule
-];
-
-const CoreModules = [FormsModule, FlexLayoutModule];
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MaterialModules, CoreModules],
-  exports: [MaterialModules, CoreModules]
+  imports: [CommonModule]
 })
 export class CoreModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SideMenuComponent } from './side-menu/side-menu.component';
+import {
+  MatToolbarModule,
+  MatSidenavModule,
+  MatCheckboxModule,
+  MatIconModule,
+  MatButtonModule,
+  MatListModule
+} from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+const MaterialModules = [
+  MatToolbarModule,
+  MatSidenavModule,
+  MatCheckboxModule,
+  MatIconModule,
+  MatButtonModule,
+  MatListModule
+];
+
+const CoreModules = [FormsModule, FlexLayoutModule];
 
 @NgModule({
   declarations: [SideMenuComponent],
-  imports: [CommonModule],
-  exports: [SideMenuComponent]
+  imports: [CommonModule, MaterialModules, CoreModules, RouterModule],
+  exports: [SideMenuComponent, MaterialModules, CoreModules]
 })
 export class SharedModule {}
