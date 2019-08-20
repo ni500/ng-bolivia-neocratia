69b65c244b216b5718af44a0ba48ee06bdfaafde
Nicolas Contreras <nicolas@pappcorn.com>
2019-08-19 21:14:05 -0500

    Main modules added to the app

---
+++ src/app/activistst/activistst.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ActiviststRoutingModule } from './activistst-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ActiviststRoutingModule
+  ]
+})
+export class ActiviststModule { }
